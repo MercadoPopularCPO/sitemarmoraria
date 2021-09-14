edce4c73a3e01f50b525d60e845ccc09e5b4a26c
Newmar Lucas <newmarlucas@gmail.com>
2021-09-14 10:28:30 -0400

    feat: add novas telas

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,11 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Contacts from './pages/Contacts'
 import Marmores from './pages/catalogo/marmores'
+import Pias from './pages/catalogo/pias'
+import Cozinhas from './pages/catalogo/cozinhas'
+import Tumulos from './pages/catalogo/tumulos'
+import Balcoes from './pages/catalogo/balcoes'
+import Nichos from './pages/catalogo/nichos'
 
 function Routes() {
     return (
@@ -14,6 +19,11 @@ function Routes() {
                 <Route path="/login" component={Login} />
                 <Route path="/contatos" component={Contacts} />
                 <Route path="/marmores" component={Marmores} />
+                <Route path="/pias" component={Pias} />
+                <Route path="/cozinhas" component={Cozinhas} />
+                <Route path="/tumulos" component={Tumulos} />
+                <Route path="/balcoes" component={Balcoes} />
+                <Route path="/nichos" component={Nichos} />
             </Switch>
         </BrowserRouter>
     )
